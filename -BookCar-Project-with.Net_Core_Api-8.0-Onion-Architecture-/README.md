7335e99b89cffaecb841fc45de670c0db35a2e04
devaeterne <info@safaksariyildiz.com>
2024-03-04 15:10:17 +0300

    entities

---
+++ -BookCar-Project-with.Net_Core_Api-8.0-Onion-Architecture-/README.md
@@ -0,0 +1,2 @@
+# -BookCar-Project-with.Net_Core_Api-8.0-Onion-Architecture-
+BookCar Project with Asp.Net Core Api 8.0 Onion Architecture 
---
+++ CoreApi8OnionArc.Domain/Entities/Banner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CoreApi8OnionArc.Domain.Entities
+{
+	public class Banner
+	{
+		public int BannerID { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public string VideoDesc { get; set; }
+
+        public string VideoUrl { get; set; }
+
+    }
+}
+
